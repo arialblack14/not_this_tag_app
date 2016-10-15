b7ac7fcb46b0686544d7f9c229ebf164f5332eec
arialblack14 <tsirasalexis@yahoo.gr>
2016-10-15 17:10:22 +0300

    fix bootstrap navbar  for layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "StaticPages", :type => :request do
   describe "Home page" do
-    it "should have the content 'Tags App'" do
+    it "should have the content 'Tag App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Tags App')
+      expect(page).to have_content('Tag App')
     end
 
     it "should have the title 'Tags'" do
